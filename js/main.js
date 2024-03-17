a32a2fd16901c8beb3c4da983384f897b3ba79b5
bzmdqz <114287249+bzmdqz@users.noreply.github.com>
2024-03-17 11:13:23 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -389,33 +389,5 @@ document.oncontextmenu = function () {
     return false;
 }
 
-//控制台输出
-//console.clear();
-let styleTitle1 = `
-font-size: 20px;
-font-weight: 600;
-color: rgb(244,167,89);
-`
-let styleTitle2 = `
-font-size:12px;
-color: #425AEF;
-`
-let styleContent = `
-color: rgb(30,152,255);
-`
-let title1 = 'Auroraの主页'
-let title2 = `
-
-██████╗ ██╗   ██╗██╗    ██╗██╗███╗   ██╗██████╗ 
-██╔══██╗╚██╗ ██╔╝██║    ██║██║████╗  ██║██╔══██╗
-██████╔╝ ╚████╔╝ ██║ █╗ ██║██║██╔██╗ ██║██║  ██║
-██╔══██╗  ╚██╔╝  ██║███╗██║██║██║╚██╗██║██║  ██║
-██████╔╝   ██║   ╚███╔███╔╝██║██║ ╚████║██████╔╝
-╚═════╝    ╚═╝    ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚═════╝               
-`
-let content = `
-博客:  https://bolo.wuhobin.top
-Github:  https://github.com/wuhobin
-`
 console.log(`%c${title1} %c${title2}
 %c${content}`, styleTitle1, styleTitle2, styleContent)
